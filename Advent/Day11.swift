c69f87b8739703d332548106cbd3bd01624678d7
Dan Guilliams <dan.guilliams@gmail.com>
2015-12-10 08:25:40 -0800

    Initial Commit

---
+++ Advent/Day11.swift
@@ -0,0 +1,20 @@
+//
+//  Day11.swift
+//  Advent
+//
+//  Created by Dan on 12/10/15.
+//  Copyright © 2015 Dan. All rights reserved.
+//
+
+import Foundation
+
+class Day11 : DayBase {
+    
+    init() {
+        super.init(day:11, content:"")
+    }
+    
+    override func DoSolve() {
+    
+    }
+}
