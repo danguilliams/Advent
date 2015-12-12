fa7318b95cfe302d6abfda47d5edc000b40f97c8
Dan Guilliams <dan.guilliams@gmail.com>
2015-12-12 09:34:17 -0800

    Day 13 prep

---
+++ Advent/Day13.swift
@@ -0,0 +1,33 @@
+//
+//  Day13.swift
+//  Advent
+//
+//  Created by Dan on 12/12/15.
+//  Copyright © 2015 Dan. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class Day13 : DayBase {
+    
+    init() {
+        super.init(day:13, fileName:"advent12.txt")
+    }
+    
+    override func DoSolve() {
+        Solve1()
+        Solve2()
+    }
+    
+    private func Solve1() {
+        
+    }
+    
+    private func Solve2() {
+        
+    }
+    
+    
+}
