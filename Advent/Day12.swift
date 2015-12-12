ef1337761c64d4f3ba4adb0aa9b3fcb49373c656
Dan Guilliams <dan.guilliams@gmail.com>
2015-12-11 20:46:37 -0800

    Day 12 prep

---
+++ Advent/Day12.swift
@@ -0,0 +1,20 @@
+//
+//  Day12.swift
+//  Advent
+//
+//  Created by Dan on 12/11/15.
+//  Copyright © 2015 Dan. All rights reserved.
+//
+
+import Foundation
+
+class Day12 : DayBase {
+    
+    init() {
+        super.init(day:12, content:"")
+    }
+    
+    override func DoSolve() {
+        
+    }
+}
