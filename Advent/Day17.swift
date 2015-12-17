27080f03b43d6038d5989e99366450bfb9b4b812
Dan Guilliams <dan.guilliams@gmail.com>
2015-12-16 19:59:42 -0800

    Day 17 Prep

---
+++ Advent/Day17.swift
@@ -0,0 +1,18 @@
+//
+//  Day17.swift
+//  Advent
+//
+//  Created by Dan on 12/16/15.
+//  Copyright © 2015 Dan. All rights reserved.
+//
+
+import Foundation
+
+
+class Day17 : DayBase {
+
+    
+    init () {
+        super.init(day: 17, fileName:"advent17.txt")
+    }
+}
