df18d11ea40b1ca2d080c7c4065a01462ca4e338
Dan Guilliams <dan.guilliams@gmail.com>
2015-12-18 19:31:34 -0800

    Day 18 prep

---
+++ Advent/Day18.swift
@@ -0,0 +1,21 @@
+//
+//  Day18.swift
+//  Advent
+//
+//  Created by Dan on 12/18/15.
+//  Copyright © 2015 Dan. All rights reserved.
+//
+
+import Foundation
+
+class Day18 : DayBase {
+
+    init () {
+        super.init(day: 18, fileName:"advent18.txt")
+    }
+    
+     override func DoSolve() {
+        //
+    }
+    
+}
