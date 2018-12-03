65f4f53ef88652d6a86fa01997f72bec3728a0c1
Dan Guilliams <daguilli@microsoft.com>
2018-12-03 12:48:29 -0800

    Day1, Part 2 complete

--- Advent2018/Day1/Day1.cs
+++ Advent2018/Day1/Day1.cs
@@ -33,6 +33,28 @@ namespace AdventOfCode2018
      * +1, +1, -2 results in  0
      * -1, -2, -3 results in -6
      * Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+     * 
+     * --- Part Two ---
+     * You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+     * 
+     * For example, using the same list of changes above, the device would loop as follows:
+     * 
+     * Current frequency  0, change of +1; resulting frequency  1.
+     * Current frequency  1, change of -2; resulting frequency -1.
+     * Current frequency -1, change of +3; resulting frequency  2.
+     * Current frequency  2, change of +1; resulting frequency  3.
+     * (At this point, the device continues from the start of the list.)
+     * Current frequency  3, change of +1; resulting frequency  4.
+     * Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+     * In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+     * 
+     * Here are other examples:
+     * 
+     * +1, -1 first reaches 0 twice.
+     * +3, +3, +4, -2, -4 first reaches 10 twice.
+     * -6, +3, +8, +5, -6 first reaches 5 twice.
+     * +7, +7, -2, -7, -4 first reaches 14 twice.
+     * What is the first frequency your device reaches twice?
      */
     public class Day1 : Day
     {
@@ -65,7 +87,36 @@ namespace AdventOfCode2018
 
         protected override string Part2()
         {
-            return "unsolved";
+            ISet<int> seenFreqs = new HashSet<int>();
+            int currentFreq = 0;
+            seenFreqs.Add(currentFreq);
+            bool duplicateFound = false;
+            while (!duplicateFound)
+            {
+                foreach (string freqStr in Input)
+                {
+                    int freq;
+                    if (int.TryParse(freqStr, out freq))
+                    {
+                        currentFreq += freq;
+                        if (seenFreqs.Contains(currentFreq))
+                        {
+                            duplicateFound = true;
+                            break;
+                        }
+                        else
+                        {
+                            seenFreqs.Add(currentFreq);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("uh oh");
+                    }
+                }
+            }
+
+            return currentFreq.ToString();
         }
 
     }
