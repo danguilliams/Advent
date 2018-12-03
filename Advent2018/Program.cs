34d9aeabe1c11bcf6c5d181b047d03724f171c7a
Dan Guilliams <daguilli@microsoft.com>
2018-12-03 12:26:08 -0800

    Day base class finished and Day1 setup

--- Advent2018/Program.cs
+++ Advent2018/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2018
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Day[] days = new Day[] { new Day1() };
+
+            Console.WriteLine("Starting solutions");
+            foreach(Day day in days)
+            {
+                day.Solve();
+            }
+            Console.WriteLine("Finished");
+            Console.WriteLine();
         }
     }
 }
