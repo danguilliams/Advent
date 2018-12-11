3526a73528c3204c7ee07c3befd1f23a38680e13
Dan Guilliams <dan.guilliams@gmail.com>
2018-12-11 08:38:39 -0800

    Day 11 pt 1, wrong answer

--- Advent2018/Day11/Day11.cs
+++ Advent2018/Day11/Day11.cs
@@ -1,4 +1,4 @@
-﻿namespace AdventOfCode2018.Day11
+﻿namespace AdventOfCode2018
 {
     /*
      * 
@@ -12,6 +12,10 @@
 
         public override int PuzzleDay => 11;
 
+        public int GridSerialNumber => 18;//1723;
+
+        public int GridSize = 300;
+
         protected override void ProcessInput()
         {
 
@@ -19,7 +23,70 @@
 
         protected override string Part1()
         {
-            return "Unfinished";
+            int arrSize = GridSize + 1;
+            int[,] grid = new int[arrSize, arrSize];
+
+            for(int y = 1; y < arrSize; y++)
+            {
+                for (int x = 1; x < arrSize ; x++)
+                {
+                    grid[x, y] = GetPowerLevel(x, y);
+                }
+            }
+
+            int maxPower = int.MinValue;
+            int maxX = 0;
+            int maxY = 0;
+            for(int y = 1; y <= GridSize - 2; y++)
+            {
+                for(int x = 1; x <= GridSize - 2; x++)
+                {
+                     int power =  grid[x, y] +     grid[x + 1, y] +     grid[x + 2, y] +
+                                  grid[x, y + 1] + grid[x + 1, y + 1] + grid[x + 2, y + 1] +
+                                  grid[x, y + 2] + grid[x + 1, y + 2] + grid[x + 2, y + 2];
+
+                    if(power > maxPower)
+                    {
+                        maxPower = power;
+                        maxX = x;
+                        maxY = y;
+                    }
+                }
+            }
+
+            return $"Max Power: {maxPower} at [{maxX},{maxY}]";
+        }
+
+        private int GetPowerLevel(int x, int y)
+        {
+            /*
+             * The power level in a given fuel cell can be found through the following process:
+             *
+             * Find the fuel cell's rack ID, which is its X coordinate plus 10.
+             * Begin with a power level of the rack ID times the Y coordinate.
+             * Increase the power level by the value of the grid serial number (your puzzle input).
+             * Set the power level to itself multiplied by the rack ID.
+             * Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+             * Subtract 5 from the power level.
+             */
+            int rackId = x * 10;
+            int power = rackId * y;
+            power = power + GridSerialNumber;
+            power = power * rackId;
+            string pStr = power.ToString();
+            // [1]23
+            
+            if(pStr.Length > 2)
+            {
+                power = int.Parse(pStr.Substring(pStr.Length - 3, 1));
+            }
+            else
+            {
+                power = 0;
+            }
+
+            power = power - 5;
+            return power;
         }
 
         protected override string Part2()
--- Advent2018/Program.cs
+++ Advent2018/Program.cs
@@ -9,16 +9,17 @@ namespace AdventOfCode2018
         {
             Day[] days = new Day[]
                 {
-                    new Day1(),
-                    new Day2(),
-                    new Day3(),
-                    new Day4(),
-                    new Day5(),
-                    new Day6(),
-                    new Day7(),
-                    new Day8(),
-                    new Day9(),
-                    new Day10(),
+                    //new Day1(),
+                    //new Day2(),
+                    //new Day3(),
+                    //new Day4(),
+                    //new Day5(),
+                    //new Day6(),
+                    //new Day7(),
+                    //new Day8(),
+                    //new Day9(),
+                    //new Day10(),
+                    new Day11(),
                 };
 
             WriteIntro(days);
