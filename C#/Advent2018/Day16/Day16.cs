c4a6834d8d7dbc37fdb75e73efbcbebcd01ab4cd
Dan Guilliams <daguilli@microsoft.com>
2018-12-17 12:16:10 -0800

    Day16 part 1 finished

--- C#/Advent2018/Day16/Day16.cs
+++ C#/Advent2018/Day16/Day16.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,65 +34,130 @@ namespace AdventOfCode2018
                 }
             }
 
-            // in i => i[1] = A, i[2] = B, i[3] = C
             //addr (add register) stores into register C the result of adding register A and register B.
             Ops.Add(new Operation("addr", (r, i) => {
-                r[i[3]] = r[i[1]] + r[i[2]];
+                r[i.C] = r[i.A] + r[i.B];
             }));
 
             //addi (add immediate) stores into register C the result of adding register A and value B.
             Ops.Add(new Operation("addi", (r, i) => {
-                r[3] = r[i[1]] + i[2];
+                r[i.C] = r[i.A] + i.B;
             }));
 
+            //mulr(multiply register) stores into register C the result of multiplying register A and register B.
             Ops.Add(new Operation("mulr", (r, i) => {
+                r[i.C] = r[i.A] * r[i.B];
             }));
 
+            //muli(multiply immediate) stores into register C the result of multiplying register A and value B.
             Ops.Add(new Operation("muli", (r, i) => {
+                r[i.C] = r[i.A] + i.B;
             }));
 
+            //banr(bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
             Ops.Add(new Operation("banr", (r, i) => {
+                r[i.C] = r[i.A] & r[i.B];
             }));
 
+            //bani(bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
             Ops.Add(new Operation("bani", (r, i) => {
+                r[i.C] = r[i.A] & i.B;
             }));
 
+            //borr(bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
             Ops.Add(new Operation("borr", (r, i) => {
+                r[i.C] = r[i.A] | r[i.B];
             }));
 
+            //bori(bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
             Ops.Add(new Operation("bori", (r, i) => {
+                r[i.C] = r[i.A] | i.B;
             }));
 
+            //setr(set register) copies the contents of register A into register C. (Input B is ignored.)
             Ops.Add(new Operation("setr", (r, i) => {
+                r[i.C] = r[i.A];
             }));
 
+            //seti(set immediate) stores value A into register C. (Input B is ignored.)
             Ops.Add(new Operation("seti", (r, i) => {
+                r[i.C] = i.A;
             }));
 
+            //gtir(greater - than immediate / register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("gtir", (r, i) => {
+                r[i.C] = i.A > r[i.B] ? 1 : 0; 
             }));
 
+            //gtri(greater - than register / immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("gtri", (r, i) => {
+                r[i.C] = r[i.A] > i.B ? 1 : 0;
             }));
 
+            //gtrr(greater - than register / register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("gtrr", (r, i) => {
+                r[i.C] = r[i.A] > r[i.B] ? 1 : 0;
             }));
 
+            //eqir(equal immediate / register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("eqir", (r, i) => {
+                r[i.C] = i.A == r[i.B] ? 1 : 0;
             }));
 
+            //eqri(equal register / immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("eqri", (r, i) => {
+                r[i.C] = r[i.A] == i.B ? 1 : 0;
             }));
 
+            //eqrr(equal register / register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
             Ops.Add(new Operation("eqrr", (r, i) => {
+                r[i.C] = r[i.A] == r[i.B] ? 1 : 0;
             }));
 
         }
 
         protected override string Part1()
         {
-            
-            throw new NotImplementedException();
+            int totalPolyopSamples = 0;
+            int[] regs = new int[] { 0, 0, 0, 0 };
+            foreach(Sample s in Samples)
+            {
+                int matches = 0;
+
+                foreach (Operation op in Ops)
+                {
+                    // init registers
+                    for (int i =0; i < 4; i++)
+                    {
+                        regs[i] = s.Before[i];
+                    }
+                    // do the op
+                    op.Execute(regs, s.Instr);
+                    // check if output matches
+                    bool isMatch = true;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (regs[i] != s.After[i])
+                        {
+                            isMatch = false;
+                            break;
+                        }
+                    }
+
+                    if (isMatch)
+                    {
+                        matches++;
+                        s.MatchingOps.Add(op.Id);
+                    }
+                }
+
+                if (matches >= 3)
+                {
+                    totalPolyopSamples++;
+                }
+            }
+
+            return $"{totalPolyopSamples} that match 3 or more opcodes";
         }
 
         protected override string Part2()
@@ -99,57 +165,52 @@ namespace AdventOfCode2018
             throw new NotImplementedException();
         }
 
-        private class IOp
-        {
-            public IOp(int[] instruction)
-            {
-                Id = instruction[0];
-                I = instruction;
-            }
-
-            public int Id;
-            public int[] I;
-            public void Execute(int[] r, Operation op)
-            {
-                op.Op(r, I);
-            }
-        }
-
+        [DebuggerDisplay("{Id}")]
         private class Operation
         {
-            public Operation(string id, Action<int[], int[]> op)
+            public Operation(string id, Action<int[], Instruction> op)
             {
                 Id = id;
-                Op = op;
+                Execute = op;
             }
-            public Action<int[], int[]> Op;
+            public Action<int[], Instruction> Execute;
             public string Id;
         }
 
         private class Sample
         {
-            public Sample(int id, string before, string op, string after)
+            public Sample(int id, string before, string instr, string after)
             {
                 Before = ParseOps(before);
-                Op = new IOp(ParseOps(op));
+                Instr = new Instruction(instr);
                 After = ParseOps(after);
+                MatchingOps = new List<string>();
+                Id = id;
             }
 
             public int Id;
             public int[] Before;
-            public IOp Op;
+            public Instruction Instr;
             public int[] After;
+            public List<string> MatchingOps;
         }
-
 
         private class Instruction
         {
             public Instruction(string instr)
             {
-                Op = ParseOps(instr);
+                P = ParseOps(instr);
+                if(P.Length != 4)
+                {
+                    throw new Exception("Incorrect Op legth!");
+                }
             }
 
-            public int[] Op;
+            private int[] P;
+            public int OpCode => P[0];
+            public int A => P[1];
+            public int B => P[2];
+            public int C => P[3];
         }
 
         public static int[] ParseOps(string s)
